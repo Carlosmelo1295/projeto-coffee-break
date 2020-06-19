f5406e3aa39a44f068c44848fb2d63a25ca0f702
Carlos Eduardo <silvamelo538@hotmail.com>
2020-06-19 19:00:58 -0300

    menu mobile

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-
+//código de confirmação de saída 
 function confirmaSaida() {
     return 'Tem certeza de que deseja sair desta página?';
 
 }
 
-window.onbeforeunload = confirmaSaida;
+window.onbeforeunload = confirmaSaida; // para chamar a função antes da página ser recarregada 
+
+//mostrar e ocultar menu mobile
+function mudar() {
+    let display = document.querySelector(".sub").style.display;
+    if (display == "none")
+        document.querySelector(".sub").style.display = 'block';
+    else
+        document.querySelector(".sub").style.display = 'none';
+}
