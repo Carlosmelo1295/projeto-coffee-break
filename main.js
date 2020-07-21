503a52b206cdcb2558c642133d2a2385e7dfdb67
Carlos <silvamelo538@hotmail.com>
2020-07-20 23:40:43 -0300

    .

---
+++ main.js
@@ -0,0 +1,20 @@
+
+//mostrar e ocultar menu mobile
+function mudar() {
+    let display = document.querySelector(".sub").style.display;
+    if (display == "none")
+        document.querySelector(".sub").style.display = 'block';
+    else {
+        document.querySelector(".sub").style.display = 'none';
+    }
+}
+
+//Submenu do menu mobile
+function mudar2() {
+    let display = document.querySelector(".subGaleria").style.display;
+    if (display == "none")
+        document.querySelector(".subGaleria").style.display = 'block';
+    else {
+        document.querySelector(".subGaleria").style.display = 'none';
+    }
+}
